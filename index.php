ed42a6ab5dd283bb57c1023b29e81eeb975003d3
João Pedro Martins de Andrade <143364783+JoaoPMA23@users.noreply.github.com>
2026-05-08 16:38:56 -0300

    As coisa q o igor fez

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Divulgação</title>
+    <link rel="stylesheet" href="assets/style.css"/>
+</head>
+<body>
+    <header>
+    <div class="logo">Book</div>
+    <nav>
+        <a href="#">Inicio</a>
+        <a href="#">Livros</a>
+        <a href="#">Categoria</a>
+        <a href="#">Contato</a>
+    </nav>
+</header>
+<hr>
+<section class="banner">
+    <div class="banner-content">
+      <h1>Descubra novos livros</h1>
+      <p>Avaliações, rankings e recomendações para leitores.</p>
+    </div>
+</section>
+<main class="container">
+    <h2 class="section-title">Livros em destaque</h2>
+
+    <div class="books">
+
+      <div class="card">
+        <img src="C:\Users\Fatec\Desktop\imagens.jpg" alt="Livro">
+        
+        <div class="card-content">
+          <h3>Dom Casmurro</h3>
+          <div class="rating">⭐ 9.1</div>
+
+          <p class="description">
+            Um clássico da literatura brasileira escrito por Machado de Assis.
+          </p>
+        </div>
+      </div>
+</body>
+</html>
